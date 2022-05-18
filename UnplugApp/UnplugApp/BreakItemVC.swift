ee338388ff7705f83955ad279dc0ee919dabaee7
Sizwe Khathi <sizwek@globalkinetic.com>
2022-05-18 03:56:03 +0200

    Initial Commit

---
+++ UnplugApp/UnplugApp/BreakItemVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnplugApp
+//
+//  Created by Sizwe Khathi on 2022/05/18.
+//
+
+import UIKit
+
+class BreakItemVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UnplugApp/UnplugApp/BreaksVC.swift
@@ -0,0 +1,17 @@
+//
+//  BreaksVC.swift
+//  UnplugApp
+//
+//  Created by Sizwe Khathi on 2022/05/18.
+//
+import UIKit
+
+class BreaksVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
