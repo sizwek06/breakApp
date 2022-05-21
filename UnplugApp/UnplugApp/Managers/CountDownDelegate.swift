d08fa95c783aa466613a1c6fa7a9ba2139b411c6
Sizwe Khathi <sizwek@globalkinetic.com>
2022-05-22 01:17:51 +0200

    Didn't tick new delegate

---
+++ UnplugApp/UnplugApp/Managers/CountDownDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  CountDownDelegate.swift
+//  UnplugApp
+//
+//  Created by Sizwe Khathi on 2022/05/22.
+//
+
+import Foundation
+
+protocol CountDownBeganDelegate: AnyObject {
+    func countDownStarted(count: String)
+}
