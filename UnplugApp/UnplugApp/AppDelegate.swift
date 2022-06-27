07c6cf3c216c3fb16edc4b4099df76e4ce990f5a
Sizwe Khathi <sizwek@globalkinetic.com>
2022-06-27 13:20:15 +0200

    Update breaks

--- UnplugApp/UnplugApp/AppDelegate.swift
+++ UnplugApp/UnplugApp/AppDelegate.swift
@@ -7,15 +7,48 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, _ in
+                print("Permission granted: \(granted)")
+                guard granted else { return }
+                self?.getNotificationSettings()
+            }
+    }
+    
+    func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { setings in
+                print("Notification settings: \(setings)")
+            }
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        registerForPushNotifications()
+        
+        let notificationOption = launchOptions?[.remoteNotification]
+        
+        if
+            let notification = notificationOption as? [String: AnyObject],
+            let aps = notification["aps"] as? [String: AnyObject] {
+            
+        }
         return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
     }
 
     // MARK: UISceneSession Lifecycle
--- UnplugApp/UnplugApp/Model/BreaksArray.swift
+++ UnplugApp/UnplugApp/Model/BreaksArray.swift
@@ -12,6 +12,5 @@ var breaksArray: [BreakItem] = [
     BreakItem(name: "Shop", breakLength: "35", colour: "black"),
     BreakItem(name: "Coffee", breakLength: "5", colour: "black"),
     BreakItem(name: "School Run", breakLength: "45", colour: "black"),
-    BreakItem(name: "Special Break", breakLength: "1", colour: "black"),
-    BreakItem(name: "Special Breakx2", breakLength: "2", colour: "black")
+    BreakItem(name: "Run", breakLength: "1", colour: "black")
 ]
